3ec81877d6ff6308e42b3780e3670b89cab56d42
maheskett <meg.heskett@gmail.com>
2018-08-03 15:27:15 -0700

    Add test for org settings log

--- tests/sentry/api/endpoints/test_organization_details.py
+++ tests/sentry/api/endpoints/test_organization_details.py
@@ -11,6 +11,7 @@ from pprint import pprint
 
 from sentry.constants import RESERVED_ORGANIZATION_SLUGS
 from sentry.models import (
+    AuditLogEntry,
     Authenticator,
     DeletedOrganization,
     Organization,
@@ -176,22 +177,22 @@ class OrganizationUpdateTest(APITestCase):
                 'organization_slug': org.slug,
             }
         )
-        response = self.client.put(
-            url,
-            data={
-                'openMembership': False,
-                'isEarlyAdopter': True,
-                'allowSharedIssues': False,
-                'enhancedPrivacy': True,
-                'dataScrubber': True,
-                'dataScrubberDefaults': True,
-                'sensitiveFields': ['password'],
-                'safeFields': ['email'],
-                'scrubIPAddresses': True,
-                'scrapeJavaScript': False,
-                'defaultRole': 'owner',
-            }
-        )
+
+        data = {
+            'openMembership': False,
+            'isEarlyAdopter': True,
+            'allowSharedIssues': False,
+            'enhancedPrivacy': True,
+            'dataScrubber': True,
+            'dataScrubberDefaults': True,
+            'sensitiveFields': ['password'],
+            'safeFields': ['email'],
+            'scrubIPAddresses': True,
+            'scrapeJavaScript': False,
+            'defaultRole': 'owner',
+        }
+
+        response = self.client.put(url, data=data)
         assert response.status_code == 200, response.content
         org = Organization.objects.get(id=org.id)
 
@@ -212,6 +213,18 @@ class OrganizationUpdateTest(APITestCase):
         assert options.get('sentry:sensitive_fields') == ['password']
         assert options.get('sentry:safe_fields') == ['email']
         assert options.get('sentry:scrape_javascript') is False
+
+        audit_log = AuditLogEntry.objects.get(organization=org)
+        assert audit_log.data['allow_joinleave'] == 'to False'
+        assert audit_log.data['early_adopter'] == 'to True'
+        assert audit_log.data['disable_shared_issues'] == 'to True'
+        assert audit_log.data['enhanced_privacy'] == 'to True'
+        assert audit_log.data['dataScrubber'] == data['dataScrubber']
+        assert audit_log.data['dataScrubberDefaults'] == data['dataScrubberDefaults']
+        assert audit_log.data['sensitiveFields'] == data['sensitiveFields']
+        assert audit_log.data['safeFields'] == data['safeFields']
+        assert audit_log.data['scrubIPAddresses'] == data['scrubIPAddresses']
+        assert 'to owner' in audit_log.data['default_role']
 
     def test_setting_legacy_rate_limits(self):
         org = self.create_organization(owner=self.user)
